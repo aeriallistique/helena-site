d7d126c0d8753793b87c34175f32a0794ed695aa
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2021-03-15 23:23:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const elements = {
     about: document.querySelector('.about_link'),
     wte: document.querySelector('.what_to_expect_link'),
     coaches: document.querySelector('.coaches_link'),
-    faq: document.querySelector('.faq_link')
+    faq: document.querySelector('.faq_link'),
+    top_div: document.querySelector('.top_div'),
+    logo: document.querySelector('.logo')
 }
 
 window.addEventListener('load', ()=>{
+    elements.logo.style.top = `${elements.top_div.clientHeight}px`;
+
     let width = window.innerWidth;
         if(width > 600){
             elements.about.classList.add('spinning-animation');
@@ -26,8 +30,7 @@ window.addEventListener('load', ()=>{
             setTimeout(()=>{elements.coaches.classList.remove('spinning-animation')},2500);
             setTimeout(()=>{elements.faq.classList.add('spinning-animation')},2500);
             setTimeout(()=>{elements.faq.classList.remove('spinning-animation')},3500);
-}
-    
+}    
 })
 window.onbeforeunload = ()=> {
     window.scrollTo(0, 0);
